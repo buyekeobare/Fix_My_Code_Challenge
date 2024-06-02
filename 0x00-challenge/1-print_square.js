2398130acbacf334b5b590a9c6ec93c3c158be85
buyekeobare <buyekeobare@gmail.com>
2024-06-02 16:27:29 -0700

    Updated file

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -15,11 +15,6 @@ if (process.argv.length <= 2) {
 
 const size = parseInt(process.argv[2], 10);
 
-if (isNaN(size) || size <= 0) {
-    process.stderr.write("Invalid size\n");
-    process.exit(1);
-}
-
 for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
         process.stdout.write("#");
